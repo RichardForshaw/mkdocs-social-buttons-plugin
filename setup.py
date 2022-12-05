01076d3bd058345858b434893e320dae2cd1f9d5
Richard Forshaw <richardforshaw@instatruck.com.au>
2022-12-05 16:39:57 +0700

    Fix TOML issues

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import find_packages, setup
 
 setup(
+    author="Richard Forshaw",
+    author_email="richard@forshaw.org",
+    url="https://github.com/richardforshaw/mkdocs-social-buttons-plugin",
     packages=find_packages(),
 )
